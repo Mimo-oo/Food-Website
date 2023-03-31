3d2516742fe8bf786e490a0ae6c18a74dfa21848
mimo-oo <fadipemichael6@gmail.com>
2023-03-31 02:07:14 -0700

    Added few things

--- main.js
+++ main.js
@@ -24,3 +24,42 @@ function food(evt, foody){
     evt.currentTarget.className += " change"
 }
 
+document.getElementById("chan").click();
+
+var currentIndex = 0;
+slideShow();
+function slideShow(){
+    var i, content, link;
+    content = document.getElementsByClassName("content");
+    link = document.getElementsByClassName('link');
+    for (i = 0; i < content.length; i ++){
+        content[i].style.display = "none";
+    }
+    for (i = 0; i < link.length; i++){
+        link[i].className = link[i].className.replace(" change", "");
+    }
+    currentIndex++
+    if (currentIndex > content.length){
+        currentIndex = 1
+    }
+    content[currentIndex - 1].style.display = "block";
+    link[currentIndex - 1].className += " change"
+    setTimeout(slideShow, 3000)
+}
+
+function validate(){
+    let name = document.getElementById("name").value;
+    let text = document.getElementById("text").value;
+    let psd = document.getElementById("psd").value;
+    let tel = document.getElementById("tel").value;
+    if (name == "" || text == ""){
+        alert("cannot be empty")
+    } else{
+        alert("Thanks for Ordering")
+    }
+    if (psd == "" || tel == ""){
+        alert("cannot be empty")
+    } else{
+        alert("Thanks for Ordering")
+    }
+}
